0adf2b6c2a3d3f1495dc39b9f8c74590b28d81ee
Chilop <ivanferrat65@gmail.com>
2024-09-30 08:23:02 -0600

    zap1223_004
    agregamos la validacion del programa2

--- P002_Valor_Absoluto/P002_Valor_Absoluto.cpp
+++ P002_Valor_Absoluto/P002_Valor_Absoluto.cpp
@@ -5,19 +5,28 @@
 
 int main() 
 {
-	float entero;
+	float valor;
 	
 	std::cout << "bienvenido al encontrador del valor absoluto de un decimal" << std::endl;
 	std::cout << "porfavor introduzca un valor decimal" << std::endl;
-	std::cin >> entero;
 
-	bool esnegativo = entero < 0;
-
-	if (esnegativo)
-	{
-		entero *= -1;
+	while (1) {
+		std::cin >> valor;
+		if (!std::cin.fail()) break;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(),
+			'\n');
+		std::cout << "porfavor introduzca un valor valido" << std::endl;
 	}
 
-	std::cout << "el valor absoluto del valor introducido es: " << entero << std::endl;
+	bool esnegativo = valor < 0;
+	
+	if (esnegativo)
+	{
+		valor *= -1;
+	}
+
+
+	std::cout << "el valor absoluto del valor introducido es: " << valor << std::endl;
 	return 0;
 }
