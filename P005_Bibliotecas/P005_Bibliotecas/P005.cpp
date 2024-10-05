07aa404ab1dde92f6a0268e15f79a8e7bc8f3649
ferratverlataia <ivanferrat65@gmail.com>
2024-10-04 21:58:20 -0600

    zap1223_007
    proyecto 5 añadido

---
+++ P005_Bibliotecas/P005_Bibliotecas/P005.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+int main() 
+{
+	int sumaenteros = 3 + 3;
+	int restaenteros = 10 - 1;
+	int multiplicacionenteros = 10 * 2;
+	int divisionenteros = 3.f / 10.f;
+	int potenciaenteros = std::pow(2,2);
+
+}
