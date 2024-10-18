4b83fd3593808980c2cfd84f29e7df1c93322f22
ferratverlataia <ivanferrat65@gmail.com>
2024-10-17 19:48:21 -0600

    zap1223_016
    fixing some stuff

---
+++ P011_Condicionales/P011_Condicionales/P011_Condicionales.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() 
+{
+	std::cout <<"Bienvenido a preguntados version de peliculas"<<std::endl;
+	std::cout << "el programa solo cuenta con el presupuesto para 5 preguntas" << std::endl;
+	std::cout<< "piensa en su"
+
+}
